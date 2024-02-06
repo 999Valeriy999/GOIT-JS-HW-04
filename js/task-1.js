0bff607b6ad5e1c56c0b733c487cc1a3d9a39cfd
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-02-06 14:47:22 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -4,9 +4,6 @@ function isEnoughCapacity(products, containerSize) {
     return totalQuantity <= containerSize;
   }
 
-bjhbhjbjhbkjnkjnjkn
-
-
   console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); 
   console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); 
   console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); 
