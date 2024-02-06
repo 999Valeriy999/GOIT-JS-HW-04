a840b1dbfad6d19655da92433bedc7fc89fdf39e
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-02-06 12:26:30 +0200

    task1

---
+++ js/task-2.js
@@ -0,0 +1,39 @@
+function calcAverageCalories(days) {
+  let sum = 0; /* створюю змінну в яку накопичуємо суму калорій */
+  const daysArr = Object.values(days); /*перетворюю обєкт в масив та виймаю з нього усі значення */
+  for (const day of daysArr) {           
+   sum += days
+  }
+}
+
+
+
+console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 3010 },
+      { day: "tuesday", calories: 3200 },
+      { day: "wednesday", calories: 3120 },
+      { day: "thursday", calories: 2900 },
+      { day: "friday", calories: 3450 },
+      { day: "saturday", calories: 3280 },
+      { day: "sunday", calories: 3300 }
+    ])
+  ); // 3180
+  
+  console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 2040 },
+      { day: "tuesday", calories: 2270 },
+      { day: "wednesday", calories: 2420 },
+      { day: "thursday", calories: 1900 },
+      { day: "friday", calories: 2370 },
+      { day: "saturday", calories: 2280 },
+      { day: "sunday", calories: 2610 }
+    ])
+  ); // 2270
+  
+  console.log(
+    calcAverageCalories([])
+  ); // 0
+
+ 
